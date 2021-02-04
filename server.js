dec1653d57fe52a522152253e7af97053ba464b9
Luis <m.luishernandez@outlook.com>
2021-02-04 13:00:25 -0500

    initiall commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
